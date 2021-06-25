a10b8d3e4d574f054b601f583f0ed5f8adf7c823
andreTYS <86248964+andreTYS@users.noreply.github.com>
2021-06-25 11:23:51 -0500

    Update Ejercicio1 !!.java

--- Ejercicio1 !!.java
+++ Ejercicio1 !!.java
@@ -5,7 +5,17 @@ Import java.util.Scanner;
 
 Public class void main(){
   Scanner teclado = new Scanner(System.in);
+  int a[][];
   int  i = 0;
   int j = 0;
-  
+  new int = a[][];
+  for (i = 0;i<=m;i++){
+    System.out.print("[ ")
+    for (j=0;j<=n;i++){
+      a[i][j] = j
+      System.out.print(a[i][j]," ")      
+    }
+    System.out.println("]")
+  }
+    
   }
